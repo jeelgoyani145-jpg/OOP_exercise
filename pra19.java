c32ba99b8d45a1c15ed5befe2012bc4474c033ba
jeelgoyani145-jpg <rekhagoyani145@gmail.com>
2026-04-30 11:42:55 +0530

    Create pra19.java

---
+++ pra19.java
@@ -0,0 +1,9 @@
+import student.Student;
+import exam.Result;
+
+public class pra19 {
+    public static void main(String[] args) {
+        Result r = new Result(1, "Ravi", 75, 82, 90);
+        r.displayResult();
+    }
+}
